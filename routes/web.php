c17791747504725783818ecd6c7d5896d5fcdf72
Reza Salehipour <rez.salehipour@gmail.com>
2023-06-01 09:00:33 +0100

    add some template files

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/2', function () {
+    return view('welcome2');
 });
